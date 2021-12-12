768cb128af3ef1d427cd3fbf686f7835b8f94774
Krlos160287 <martigracia2018@gmail.com>
2021-12-12 12:08:50 +0000

    Finished

--- src/js/component/counter.js
+++ src/js/component/counter.js
@@ -1,28 +1,66 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from "prop-types";
+import moment from "moment";
 
 export const Counter = (props) => {
-	return (
-		<div className="container">
-			<div className="first">
-				<div className="center"></div>
-				<i className="fas fa-clock fa-5x"></i>
+	if (count > 0) {
+		return (
+			<div className="container1">
+				<div className="center1">Hasta Nochebuena quedan...</div>
+				<div className="container">
+					<div className="clock">
+						<div className="center"></div>
+						<i className="fas fa-clock fa-5x"></i>
+					</div>
+					<div className="clock">
+						<div className="center">{props.days} Días</div>
+					</div>
+					<div className="clock">
+						<div className="center">{props.hours % 24} Horas</div>
+					</div>
+					<div className="clock">
+						<div className="center">
+							{props.minutes % 60} Minutos
+						</div>
+					</div>
+					<div className="clock">
+						<div className="center">
+							{props.seconds % 60} Segundos
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="fourth">
-				<div className="center">{props.days}</div>
+		);
+	} else {
+		return (
+			<div className="container1">
+				<div className="center1">¡¡¡Llegó Nochebuena!!!</div>
+				<div className="container">
+					<div className="clock">
+						<div className="center"></div>
+						<i className="fas fa-clock fa-5x"></i>
+					</div>
+					<div className="clock">
+						<div className="center">{props.days} Días</div>
+					</div>
+					<div className="clock">
+						<div className="center">{props.hours % 24} Horas</div>
+					</div>
+					<div className="clock">
+						<div className="center">
+							{props.minutes % 60} Minutos
+						</div>
+					</div>
+					<div className="clock">
+						<div className="center">
+							{props.seconds % 60} Segundos
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="fifth">
-				<div className="center">{props.hours % 24}</div>
-			</div>
-			<div className="sixth">
-				<div className="center">{props.minutes % 60}</div>
-			</div>
-			<div className="seventh">
-				<div className="center">{props.seconds % 60}</div>
-			</div>
-		</div>
-	);
+		);
+	}
 };
 
 Counter.propTypes = {
@@ -32,16 +70,36 @@ Counter.propTypes = {
 	seconds: PropTypes.number,
 };
 
-var count = 17800;
-setInterval(function () {
-	const two = Math.floor(count / 24 / 60 / 60);
-	const three = Math.floor(count / 60 / 60);
-	const four = Math.floor(count / 60);
-	const five = Math.floor(count / 1);
+var a = moment([2021, 11, 24, 0, 0, 0]);
+var b = moment([]);
+var duration = moment.duration(a.diff(b, "second"));
+console.log(duration);
 
-	count--;
-	ReactDOM.render(
-		<Counter seconds={five} minutes={four} hours={three} days={two} />,
-		document.querySelector("#app")
-	);
-}, 1000);
+var count = duration;
+if (count > 0) {
+	setInterval(function () {
+		const two = Math.floor(count / 24 / 60 / 60);
+		const three = Math.floor(count / 60 / 60);
+		const four = Math.floor(count / 60);
+		const five = Math.floor(count / 1);
+
+		count--;
+		ReactDOM.render(
+			<Counter seconds={five} minutes={four} hours={three} days={two} />,
+			document.querySelector("#app")
+		);
+	}, 1000);
+} else {
+	setInterval(function () {
+		const two = 0;
+		const three = 0;
+		const four = 0;
+		const five = 0;
+
+		count--;
+		ReactDOM.render(
+			<Counter seconds={five} minutes={four} hours={three} days={two} />,
+			document.querySelector("#app")
+		);
+	}, 1000);
+}
